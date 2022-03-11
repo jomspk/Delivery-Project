16acb4d268414ca71ace9a1a25e31988d41e61da
BanriYasui <dontstop1110@gmail.com>
2022-03-11 16:05:14 +0900

    Update acceleration.ino
    重力加速度を考慮した値に変換

--- acceleration/acceleration.ino
+++ acceleration/acceleration.ino
@@ -18,17 +18,34 @@ void Initialization(){
   }
 void loop() {
   // put your main code here, to run repeatedly:
+  // 地球の重力である1Gの加速度
+  float ms2 = 9.80665;
 
-  int x = analogRead(Out_x);
-  int y = analogRead(Out_y);
-  int z = analogRead(Out_z);
+  // 電源電圧3.3V時のオフセット電圧
+  float offset_voltage = 1.650;
 
-  //Serial.print("X:");
-  //Serial.println(x);
+  // XYZの電圧を取得する
+  float x = (analogRead(Out_x)/1024.0)*5.0*1000;
+  float y = (analogRead(Out_y)/1024.0)*5.0*1000;
+  float z = (analogRead(Out_z)/1024.0)*5.0*1000;
+
+  // XYZからオフセット電圧を引いた電圧を求める
+  x = x - offset_voltage;
+  y = y - offset_voltage;
+  z = z - offset_voltage;
+
+  // XYZから重力を求める
+  float xg = x/1000.0;
+  float yg = y/1000.0;
+  float zg = z/1000.0;
+
+  // XYZの重力から加速度を算出して出力する
+  Serial.print("X:");
+  Serial.print(xg*ms2);
   Serial.print("Y:");
-  Serial.println(y);
-  //Serial.print("Z:");
-  //Serial.println(z);
+  Serial.print(yg*ms2);
+  Serial.print("Z:");
+  Serial.println(zg*ms2);
   delay(100);
   
 }
